4bba05e79d04ad6c6350c9dc93ed9e892c8e7c80
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-12-12 14:19:21 +0100

    Added choose, start and store.fxml files. THeese now run in main under application.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,53 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public Button findRecipiesButton;
+    public Button storeIngredientButton;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent parent = (Parent) FXMLLoader.load(getClass().getResource(
-                "/view/MainPane.fxml"));
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
-        stage.setTitle("Ingredients");
-        stage.show();
-        
+    public void start(Stage primaryStage) throws Exception {
+        Stage root = FXMLLoader.load(getClass().getResource("/view/start.fxml"));
+        root.show();
+
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+
+    public void popupChooseIngredients (javafx.event.ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/chooseIngredients.fxml"));
+        Stage root = fxmlLoader.load();
+        root.show();
+
+    }
+
+    public void popupStoreIngredient (javafx.event.ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MainPane.fxml"));
+        AnchorPane root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void closeStoreIngredientsPopup() {
+        Stage stage = (Stage) storeIngredientButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void changeSceneCloseChooseIngredientsPopup() {
+        Stage stage = (Stage) findRecipiesButton.getScene().getWindow();
+        stage.close();
     }
 }
