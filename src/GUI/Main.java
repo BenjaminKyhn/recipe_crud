9c801b777bb64e981ad3316f83bef3e4a9f471e1
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-16 10:07:01 +0100

    Modality

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,8 +21,9 @@ public class Main extends Application {
 
     public void popupChooseIngredients (javafx.event.ActionEvent actionEvent) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI/chooseIngredients.fxml"));
-        Stage root = fxmlLoader.load();
-        root.show();
+        Stage popUp = fxmlLoader.load();
+        popUp.initModality(Modality.APPLICATION_MODAL);
+        popUp.show();
     }
 
     public void popupStoreIngredient (javafx.event.ActionEvent actionEvent) throws Exception {
@@ -29,6 +31,7 @@ public class Main extends Application {
         AnchorPane anchorPane = fxmlLoader.load();
         Scene scene = new Scene(anchorPane);
         Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
         stage.setScene(scene);
         stage.show();
     }
